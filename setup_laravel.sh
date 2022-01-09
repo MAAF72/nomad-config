9a6482826c86fa67538d478feea26743c519994a
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-09 20:24:53 +0700

    update setup_laravel.sh

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+if test -d "/var/www/public"; then
+    rm -rf /var/www/public
+fi
 composer create-project --prefer-dist laravel/laravel /var/www/
 chown -R www-data.www-data /var/www/storage
 chown -R www-data.www-data /var/www//bootstrap/cache
