b313efd04e3ab216b1d9e26e04d9340219fd6231
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-14 14:10:27 +0700

    update setup_reverse_proxy.sh

--- setup_reverse_proxy.sh
+++ setup_reverse_proxy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-sudo ufw allow $WEB_APP_PORT/tcp
+export
+
+sudo ufw allow ${WEB_APP_PORT-80}/tcp
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/reverse_proxy
-sed -i "s!||WEB_APP_PORT||!$WEB_APP_PORT!g" reverse_proxy
+sed -i "s!||WEB_APP_PORT||!${WEB_APP_POR-80}T!g" reverse_proxy
 sed -i "s!||WEB_APP_DOMAIN||!${WEB_APP_DOMAIN-_}!g" reverse_proxy
-sed -i "s!||PROXY_ADDRESS_PORT||!http://$PROXY_ADDRESS:$PROXY_PORT!g" reverse_proxy
+sed -i "s!||PROXY_ADDRESS_PORT||!http://${PROXY_ADDRESS-127.0.0.1}:${PROXY_PORT-80}!g" reverse_proxy
 
 if test -f "/etc/nginx/sites-enabled/default"; then
     sudo rm /etc/nginx/sites-enabled/default
@@ -14,7 +16,7 @@ if test -f "/etc/nginx/sites-available/default"; then
     sudo rm /etc/nginx/sites-available/default
 fi
 
-sudo cp reverse_proxy /etc/nginx/sites-available/reverse_proxy_$PROXY_ADDRESS:$PROXY_PORT:${WEB_APP_DOMAIN-_}
-sudo ln -s /etc/nginx/sites-available/reverse_proxy_$PROXY_ADDRESS:$PROXY_PORT:${WEB_APP_DOMAIN-_} /etc/nginx/sites-enabled
+sudo cp reverse_proxy /etc/nginx/sites-available/rp_${APPLICATION_ID--}_${WEB_APP_DOMAIN--}
+sudo ln -s /etc/nginx/sites-available/rp_${APPLICATION_ID--}_${WEB_APP_DOMAIN--} /etc/nginx/sites-enabled/
 rm reverse_proxy
 sudo nginx -s reload
