ab510bdd21a15cd50a791c0e4c58f9bb4c691a5b
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-16 16:19:41 +0700

    fix typo setup_reverse_proxy.sh

--- setup_reverse_proxy.sh
+++ setup_reverse_proxy.sh
@@ -5,7 +5,7 @@ CFG_NAME=rp_${APPLICATION_ID--}_${WEB_APP_DOMAIN--}
 
 sudo ufw allow ${WEB_APP_PORT-80}/tcp
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/reverse_proxy
-sed -i "s!||WEB_APP_PORT||!${WEB_APP_POR-80}!g" reverse_proxy
+sed -i "s!||WEB_APP_PORT||!${WEB_APP_PORT-80}!g" reverse_proxy
 sed -i "s!||WEB_APP_DOMAIN||!${WEB_APP_DOMAIN-_}!g" reverse_proxy
 sed -i "s!||PROXY_ADDRESS_PORT||!http://${PROXY_ADDRESS-127.0.0.1}:${PROXY_PORT-80}!g" reverse_proxy
 
