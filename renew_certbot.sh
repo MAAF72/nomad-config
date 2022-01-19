86f34737d4877a3f13822e5d3440a504a443d27c
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-19 17:51:09 +0700

    update setup and renew certbot

--- renew_certbot.sh
+++ renew_certbot.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-CERTBOT_NAME=cb_${APPLICATION_ID--}_${WEB_APP_DOMAIN--}
 sudo certbot --nginx certonly --non-interactive -d $WEB_APP_DOMAIN --force-renewal
 
 sudo nginx -t && sudo nginx -s reload
+
+crontab -l
+
+if test -f "/etc/cron.d/certbot"; then
+    sudo cat /etc/cron.d/certbot
+fi
--- setup_certbot.sh
+++ setup_certbot.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-CERTBOT_NAME=cb_${APPLICATION_ID--}_${WEB_APP_DOMAIN--}
-sudo certbot --certbot-name $CERTBOT_NAME --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WEB_APP_DOMAIN
+sudo certbot --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WEB_APP_DOMAIN
 
 sudo nginx -t && sudo nginx -s reload
 
