d99ecded374ecf082b6fff5cca76a2bbd4c18bea
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-19 17:35:21 +0700

    update setup_certbot.sh

--- setup_certbot.sh
+++ setup_certbot.sh
@@ -4,3 +4,9 @@ CERTBOT_NAME=cb_${APPLICATION_ID--}_${WEB_APP_DOMAIN--}
 sudo certbot --certbot-name $CERTBOT_NAME --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WEB_APP_DOMAIN
 
 sudo nginx -t && sudo nginx -s reload
+
+crontab
+
+if test -f "/etc/cron.d/certbot"; then
+    sudo cat /etc/cron.d/certbot
+fi
