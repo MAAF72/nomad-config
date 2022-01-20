f6495ca7765a97419b3e4dfb9cbefe9c982dc93b
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-20 08:27:26 +0700

    add server-start-hook service

---
+++ server-start-hook.py
@@ -0,0 +1,20 @@
+import requests
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Server start hook.')
+
+parser.add_argument('--endpoint', type=str, required=True)
+args = parser.parse_args()
+
+while True:
+    t = time.localtime()
+    current_time = time.strftime("%d/%m/%Y %H:%M:%S", t)
+    
+    resp = requests.get(args.endpoint)
+
+    if resp.ok:
+        print(f'{current_time} => {resp.json()}')
+        break
+    else:
+        time.sleep(2 * 60) # Sleep for 10 minutes
