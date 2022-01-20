3ae12683367800845e97e4f7b17cf3ba1282a4b4
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-20 12:18:54 +0700

    update server-start-hook service

--- server-start-hook.py
+++ server-start-hook.py
@@ -5,13 +5,14 @@ import argparse
 parser = argparse.ArgumentParser(description='Server start hook.')
 
 parser.add_argument('--endpoint', type=str, required=True)
+parser.add_argument('--api-key', type=str, required=True)
 args = parser.parse_args()
 
 while True:
     t = time.localtime()
     current_time = time.strftime("%d/%m/%Y %H:%M:%S", t)
     
-    resp = requests.get(args.endpoint)
+    resp = requests.get(args.endpoint, { 'api-key': args.api_key })
 
     if resp.ok:
         print(f'{current_time} => {resp.json()}')
