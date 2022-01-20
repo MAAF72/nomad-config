d6beb1ca7214ef553856c716f9b4650e174d29e0
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-20 07:11:12 +0700

    update auto_renew_certbot.sh

--- auto_renew_certbot.sh
+++ auto_renew_certbot.sh
@@ -2,8 +2,12 @@
 
 CB_CFG_FILE=/etc/letsencrypt/renewal/$WEB_APP_DOMAIN.conf
 
+# Asumsi jika sudah terinstall certbot, pasti masih manual renew
 if test -f "$CB_CFG_FILE.disabled"; then
     sudo mv $CB_CFG_FILE.disabled $CB_CFG_FILE
-fi
 
-sudo certbot certonly --nginx --non-interactive -d $WEB_APP_DOMAIN --force-renewal
+    sudo certbot certonly --nginx --non-interactive -d $WEB_APP_DOMAIN --force-renewal
+else
+    # Install jika belum terinstall, set auto renew
+    sudo certbot --nginx --non-interactive --agree-tos -m $CERTBOT_EMAIL -d $WEB_APP_DOMAIN
+fi
