cb19d09c4b23dae617e98616c95ab9f2a463782b
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-06 13:38:45 +0700

    update reverse_proxy

--- setup_reverse_proxy.sh
+++ setup_reverse_proxy.sh
@@ -7,8 +7,12 @@ sudo ufw allow ${WEB_APP_PORT-80}/tcp
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/reverse_proxy
 sed -i "s!||WEB_APP_PORT||!${WEB_APP_PORT-80}!g" reverse_proxy
 sed -i "s!||WEB_APP_DOMAIN||!${WEB_APP_DOMAIN-_}!g" reverse_proxy
-sed -i "s!||PROXY_ADDRESS_PORT||!http://${PROXY_ADDRESS-127.0.0.1}:${PROXY_PORT-80}!g" reverse_proxy
-sed -i "s!||APPLICATION_NAME||!${APPLICATION_NAME-}!g" reverse_proxy
+
+if test -n "${APPLICATION_DIR-}"; then
+    sed -i "s!||PROXY_ADDRESS_PORT||!http://${PROXY_ADDRESS-127.0.0.1}:${PROXY_PORT-80}/${APPLICATION_DIR-}!g" reverse_proxy
+else
+    sed -i "s!||PROXY_ADDRESS_PORT||!http://${PROXY_ADDRESS-127.0.0.1}:${PROXY_PORT-80}!g" reverse_proxy
+fi
 
 if test -f "/etc/nginx/sites-enabled/default"; then
     sudo rm /etc/nginx/sites-enabled/default
