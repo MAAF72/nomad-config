04ddc2206705213be0cdaf6835464675ad74e3e2
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-06 10:38:00 +0700

    update setup_laravel.sh

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -6,11 +6,12 @@ mkdir -p $INSTALLATION_DIR
 cd $INSTALLATION_DIR
 
 composer create-project --prefer-dist laravel/laravel $INSTALLATION_DIR
-chown -R www-data.www-data /var/www/storage
-chown -R www-data.www-data /var/www/bootstrap/cache
+chown -R www-data.www-data $INSTALLATION_DIR/storage
+chown -R www-data.www-data $INSTALLATION_DIR/bootstrap/cache
 
-mv . /var/www
+cp -a $INSTALLATION_DIR /var/www
 
 cd $HOME
+
 rm -drf $INSTALLATION_DIR
-rm -drf /var/tmp
+rm -f /var/tmp/setup_laravel.sh
