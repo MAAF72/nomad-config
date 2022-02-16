194207ee2b4dcf969f835394a784609da6975113
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-17 06:34:20 +0700

    update custom-wp-config.php

--- custom-wp-config.php
+++ custom-wp-config.php
@@ -4,5 +4,7 @@ if ( !empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PR
 
 if ( isset($_SERVER['HTTP_X_FORWARDED_HOST']) ) {
     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], '/', 1));
+    if (!(bool)ip2long($_SERVER['HTTP_HOST'])) {
+        $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], '/', 1));
+    }
 }
