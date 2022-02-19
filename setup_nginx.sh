bb88b3d9b58795f0431c8f4051238f21869b2cbc
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-19 12:00:41 +0700

    update setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -4,7 +4,7 @@
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/nginx_web_config
 
 mkdir -p $WEB_SERVER_DIR/sites-available
-cp nginx_default_web_config $WEB_SERVER_DIR/sites-available/default
+cp nginx_web_config $WEB_SERVER_DIR/sites-available/default
 
 # Installing default nginx web config for ingress
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/setup_reverse_proxy.sh
@@ -13,5 +13,5 @@ chmod +x setup_reverse_proxy.sh
 ./setup_reverse_proxy.sh
 
 # Remove left over files
-rm nginx_default_web_config
+rm nginx_web_config
 rm setup_reverse_proxy.sh
