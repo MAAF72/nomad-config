bb9f332288a2a3ddb87ad012e2f5cf587c078020
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-19 12:54:55 +0700

    update setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 
-whoami
-
-# Installing default nginx web config for container
-curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/nginx_web_config
 
 mkdir -p $WEB_SERVER_DIR/sites-available
-sudo cp nginx_web_config $WEB_SERVER_DIR/sites-available/default
+
+# Prevent default file created as folder
+if test -d "$WEB_SERVER_DIR/sites-available/default"; then
+    sudo rm -drf $WEB_SERVER_DIR/sites-available/default
+fi
+
+# Installing default nginx web config for container
+if ! test -f "$WEB_SERVER_DIR/sites-available/default"; then
+    curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/nginx_web_config
+    sudo cp nginx_web_config $WEB_SERVER_DIR/sites-available/default
+fi
 
 # Installing default nginx web config for ingress
 curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/setup_reverse_proxy.sh
