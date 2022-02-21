1f46de6f969d0e89331a1fccf329584d0bb66b98
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-21 08:25:44 +0700

    update setup_empty_application.sh

--- setup_empty_application.sh
+++ setup_empty_application.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
-mkdir -p $APPLICATION_DIR
-cd $APPLICATION_DIR
 
-mkdir -p public
+mkdir -p $APPLICATION_DIR/public
 
-cd public
-
-curl -fsSL https://raw.githubusercontent.com/MAAF72/nomad-config/master/paas-landing.html -o index.html
-
-rm -f /var/tmp/setup_empty_web_appliaction.sh
+curl -fsSL https://raw.githubusercontent.com/MAAF72/nomad-config/master/paas-landing.html -o $APPLICATION_DIR/public/index.html
