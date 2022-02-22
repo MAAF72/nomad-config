2941580e117f7ceed5711104f1b99a98974532fa
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-22 15:25:38 +0700

    update setup_lap.sh

--- setup_lap.sh
+++ setup_lap.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/bash
 
-mkdir -p $APPLICATION_DIR/letsencrypt
-mkdir -p $APPLICATION_DIR/www/public
-mkdir -p $APPLICATION_DIR/apache2/sites-available
-mkdir -p $APPLICATION_DIR/apache2/sites-enabled
+mkdir -p $WEB_SERVER_DIR/letsencrypt
+mkdir -p $WEB_SERVER_DIR/www/public
+
+# Prevent default file created as folder
+if test -d "$WEB_SERVER_DIR/sites-available/default"; then
+    sudo rm -drf $WEB_SERVER_DIR/sites-available/default
+fi
+
+# Installing default apache web config for container
+if ! test -f "$WEB_SERVER_DIR/sites-available/default"; then
+    curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/apache_web_config
+    sudo cp apache_web_config $WEB_SERVER_DIR/sites-available/default
+fi
+
+# Installing default apache web config for ingress
+curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/setup_reverse_proxy.sh
+
+chmod +x setup_reverse_proxy.sh
+./setup_reverse_proxy.sh
+
+# Remove left over files
+rm -f apache_web_config
+rm -f setup_reverse_proxy.sh
