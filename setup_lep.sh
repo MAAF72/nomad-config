f7293f5ae47527325135db427a1c831207065bda
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-23 14:59:48 +0700

    update setup_lep.sh

--- setup_lep.sh
+++ setup_lep.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/bash
 
-mkdir -p $APPLICATION_DIR/letsencrypt
-mkdir -p $APPLICATION_DIR/www/public
-mkdir -p $APPLICATION_DIR/nginx/sites-available
-mkdir -p $APPLICATION_DIR/nginx/sites-enabled
+mkdir -p $WEB_SERVER_DIR/letsencrypt
+mkdir -p $WEB_SERVER_DIR/www/public
+mkdir -p $WEB_SERVER_DIR/sites-available
+
+# Prevent default file created as folder
+if test -d "$WEB_SERVER_DIR/sites-available/default"; then
+    sudo rm -drf $WEB_SERVER_DIR/sites-available/default
+fi
+
+# Installing default nginx web config for container
+if ! test -f "$WEB_SERVER_DIR/sites-available/default"; then
+    curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/nginx_web_config
+    sudo cp nginx_web_config $WEB_SERVER_DIR/sites-available/default
+fi
+
+# Installing default nginx web config for ingress
+curl --silent --remote-name https://raw.githubusercontent.com/MAAF72/nomad-config/master/setup_reverse_proxy.sh
+
+chmod +x setup_reverse_proxy.sh
+./setup_reverse_proxy.sh
+
+# Remove left over files
+rm -f nginx_web_config
+rm -f setup_reverse_proxy.sh
