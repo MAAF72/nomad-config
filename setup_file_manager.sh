f388b381882509f43256c4446eabcc5589689516
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-25 21:32:20 +0700

    update setup_file_manager.sh

--- setup_file_manager.sh
+++ setup_file_manager.sh
@@ -8,10 +8,10 @@ unzip $FILE_MANAGER_DIR/file-manager.zip
 # Do sed for admin creds
 START="# START: FILE_MANAGER_ADMIN_CREDENTIAL #"
 END="# END: FILE_MANAGER_ADMIN_CREDENTIAL #"
-ADMIN_CREDENTIAL="    '$ADMIN_USERNAME' => password_hash('$ADMIN_PASSWORD', PASSWORD_DEFAULT)"
+FM_ADMIN_CREDENTIAL="    '$FM_ADMIN_USERNAME' => password_hash('$FM_ADMIN_USERNAME', PASSWORD_DEFAULT)"
 
 sudo csplit $FILE_MANAGER_DIR/index.php '/'"$START"'/+1' '/'"$END"'/' &>/dev/null
-(cat xx00; echo "$ADMIN_CREDENTIAL"; cat xx02) | sudo tee $FILE_MANAGER_DIR/index.php
+(cat xx00; echo "$FM_ADMIN_CREDENTIAL"; cat xx02) | sudo tee $FILE_MANAGER_DIR/index.php
 rm -f xx00 xx01 xx02
 
 rm -f $FILE_MANAGER_DIR/file-manager.zip
