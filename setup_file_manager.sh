6e7da0eb9be0c7c1d998e4eda51e5a4eb607903e
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-25 21:51:28 +0700

    update setup_file_manager.sh

--- setup_file_manager.sh
+++ setup_file_manager.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-mkdir -p $FILE_MANAGER_DIR
+mkdir -p $FM_DIR
 
-curl -fksSL https://raw.githubusercontent.com/MAAF72/nomad-config/master/file-manager.zip -o $FILE_MANAGER_DIR/file-manager.zip
-unzip $FILE_MANAGER_DIR/file-manager.zip
+curl -fksSL https://raw.githubusercontent.com/MAAF72/nomad-config/master/file-manager.zip -o $FM_DIR/file-manager.zip
+unzip $FM_DIR/file-manager.zip
 
 # Do sed for admin creds
 START="# START: FILE_MANAGER_ADMIN_CREDENTIAL #"
@@ -11,11 +11,11 @@ END="# END: FILE_MANAGER_ADMIN_CREDENTIAL #"
 FM_ADMIN_CREDENTIAL="    '$FM_ADMIN_USERNAME' => password_hash('$FM_ADMIN_USERNAME', PASSWORD_DEFAULT)"
 
 sudo csplit $FILE_MANAGER_DIR/index.php '/'"$START"'/+1' '/'"$END"'/' &>/dev/null
-(cat xx00; echo "$FM_ADMIN_CREDENTIAL"; cat xx02) | sudo tee $FILE_MANAGER_DIR/index.php
+(cat xx00; echo "$FM_ADMIN_CREDENTIAL"; cat xx02) | sudo tee $FM_DIR/index.php
+
 rm -f xx00 xx01 xx02
+rm -f $FM_DIR/file-manager.zip
 
-rm -f $FILE_MANAGER_DIR/file-manager.zip
-
-if [ -d "$FILE_MANAGER_DIR" ]; then
-    sudo chown -R www-data:www-data $FILE_MANAGER_DIR
+if [ -d "$FM_DIR" ]; then
+    sudo chown -R www-data:www-data $FM_DIR
 fi
