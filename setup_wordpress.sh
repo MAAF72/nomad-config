91feb4455e511fab5e8e1a3cd19add48fbbee5c5
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-02-25 08:37:49 +0700

    update setup_wordpress.sh and apache_web_config_application

--- setup_wordpress.sh
+++ setup_wordpress.sh
@@ -10,19 +10,22 @@ cd wordpress_tmp
 
 curl -O https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
-touch wordpress/.htaccess
 cp wordpress/wp-config-sample.php wordpress/wp-config.php
+
+# Configure htaccess
+curl "https://raw.githubusercontent.com/MAAF72/nomad-config/master/wordpress.htaccess" -o wordpress/.htaccess
+sed -i "s!||APPLICATION_NAME||!${APPLICATION_NAME-}!g" wordpress/.htaccess
 
 # Configure salts
 curl "https://api.wordpress.org/secret-key/1.1/salt/" -o salts
 csplit wordpress/wp-config.php '/AUTH_KEY/' '/NONCE_SALT/+1'
-cat xx00 salts xx02 > wordpress/wp-config.php
+cat xx00 salts xx02 | tee wordpress/wp-config.php
 rm -f salts xx00 xx01 xx02
 
 # Configure custom wp-config
 curl "https://raw.githubusercontent.com/MAAF72/nomad-config/master/custom-wp-config.php" -o custom-wp-config.php
 csplit wordpress/wp-config.php '/Add any custom values between this line and the "stop editing" line./+1' "/That's all, stop editing! Happy publishing./-1"
-cat xx00 custom-wp-config.php xx02 > wordpress/wp-config.php
+cat xx00 custom-wp-config.php xx02 | tee wordpress/wp-config.php
 rm -f xx00 xx01 xx02 custom-wp-config.php
 
 # Configure db using env
